adbb9a9ea4d7badf4922d6b72767ae854c7f440b
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-04-06 23:00:10 +0530

    new file added

--- Scripting/images.py
+++ Scripting/images.py
@@ -1,9 +1,14 @@
 from PIL import Image, ImageFilter
 
-img =  Image.open('./Pokedex/206_pikachu.jpg')
+img = Image.open('./Pokedex/206_pikachu.jpg')
 
 print(img.format)
-filtered_blur_image =img.filter(ImageFilter.BLUR)
+filtered_blur_image = img.filter(ImageFilter.BLUR)
 filtered_blur_image.save("blur_pikachu.png", 'png')
+
 filtered_smooth_image = img.filter(ImageFilter.SMOOTH)
 filtered_smooth_image.save("smooth_pikachu.png", 'png')
+
+filtered_converted_image = img.convert('L')  # L means grayscale
+filtered_converted_image.save("grayconvertedimage.png", 'png')
+
