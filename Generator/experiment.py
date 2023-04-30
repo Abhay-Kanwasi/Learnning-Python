26ad15a87ddbb769a76d7f0c91649fe50b34a78d
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-04-30 20:58:15 +0530

    generator knowledge gained

---
+++ Generator/experiment.py
@@ -0,0 +1,5 @@
+# It's create the items one by one
+print(range(100))
+
+# It's create a giant list of 100 items in our computers memory
+print(list(range(100)))
--- Generator/initial.py
+++ Generator/initial.py
@@ -1,1 +1,13 @@
-# Generate allow us to generate a sequence of values
+# Generate allow us to generate a sequence of values over time.
+
+print(list(range(10)))
+
+
+def makelist(num):
+    result = []
+    for i in range(num):
+        result.append(i*2)
+    return result
+
+my_list = makelist(100)
+print(my_list)
