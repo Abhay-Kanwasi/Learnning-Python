f067f5bc7c82dde70d331968dea36862e2bd358a
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-05-17 22:16:01 +0530

    check the website title

---
+++ Automation Testing/automation2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+firefox_service = Service('geckodriver.exe')
+
+firefox_browser = webdriver.Firefox(service=firefox_service)
+
+firefox_browser.maximize_window()
+firefox_browser.get('https://www.seleniumeasy.com/')
+
+assert 'Learno Selenium with Best Practices and Examples | Selenium Easy' in firefox_browser.title
