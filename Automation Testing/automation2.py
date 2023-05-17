9d2c9d49ec8b5cf8fcac735597f31972381c6a93
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-05-17 22:53:08 +0530

    grab element by class name

--- Automation Testing/automation2.py
+++ Automation Testing/automation2.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 
 firefox_service = Service('geckodriver.exe')
-
 firefox_browser = webdriver.Firefox(service=firefox_service)
 
 firefox_browser.maximize_window()
 firefox_browser.get('https://www.seleniumeasy.com/')
 
 assert 'Learn Selenium with Best Practices and Examples | Selenium Easy' in firefox_browser.title
+
+button = firefox_browser.find_element(By.CLASS_NAME, 'form-control')
+print(button.get_attribute('innerHTML'))
