ec5e8cc45eee12dde87c16fb82b21b69c37d506e
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-05-26 23:17:35 +0530

    more file i/o operation

--- File Handling/IO/script.py
+++ File Handling/IO/script.py
@@ -1,6 +1,19 @@
-my_file = open('test.txt')
-print(my_file.read())
-print(my_file.readline())
-print(my_file.seek(0))
-print(my_file.readlines())
-my_file.close()
+# my_file = open('test.txt')
+# print(my_file.read())
+# print(my_file.readline())
+# print(my_file.seek(0))
+# print(my_file.readlines())
+# my_file.close()
+
+# Standard way to read a file
+# with open('test.txt',mode='a') as my_file:
+#     text = my_file.write("\nI'm a computer science student.")
+#     print(my_file.readlines())
+
+
+
+# Created a new file if it doesn't exist.
+with open('creation.txt',mode='w') as creation:
+    text = creation.write("We are good people.")
+    print(creation)
+
