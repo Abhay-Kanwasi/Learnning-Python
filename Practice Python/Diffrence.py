429b5cc1859c9f18dd7d023085b3a6ab0c732e65
Abhay Kanwasi <abhaykanwasi@itgopeshwar.ac.in>
2023-06-17 23:35:31 +0530

    Create Diffrence.py

---
+++ Practice Python/Diffrence.py
@@ -0,0 +1,14 @@
+# first number
+num1 = 25
+# second number
+num2 = 13
+
+# num1 is greater than num2
+if num1 > num2:
+    diff = num1 - num2
+# num1 is less than num2
+else:
+    diff = num2 - num1
+
+# print difference value
+print('The difference between 
