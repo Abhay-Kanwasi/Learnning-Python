ff6c6e598aca78dfc84bb10ed6489175cc4b7222
Abhay Kanwasi <abhaykanwasi@itgopeshwar.ac.in>
2023-06-18 22:01:55 +0530

    lower case upper case

---
+++ Practice Python/case.py
@@ -0,0 +1,7 @@
+# Python program to convert uppercase to lowercase
+
+#take input
+string = input('Enter any string: ')
+  
+# lower() function to convert uppercase to lowercase
+print('In Upper Case:', string.lower())
