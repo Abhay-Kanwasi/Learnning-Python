b3353d7dd42abd3e6fe785f5e0398df8ae91267e
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-14 20:03:56 +0530

    Program 5 added

---
+++ 100 Program/Program 5.py
@@ -0,0 +1,10 @@
+# Average of 2 numbers
+
+number1 = int(input("Enter the number : "))
+number2 = int(input("Enter the number : "))
+
+# average = (number1+number2)/number of inputs
+
+average = (number1 + number2) / 2
+
+print(f'Average of {number1} and {number2} is {average}.')
