99200ff8ee703ec6b093f584dbf017d89cbcea53
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-14 20:02:55 +0530

    program 1 added

---
+++ 100 Program/Program 1.py
@@ -0,0 +1,8 @@
+# Add two numbers
+
+number1 = int(input("Enter the number : "))
+number2 = int(input("Enter the number : "))
+
+addition = number1 + number2
+
+print(f'Addition of {number1} and {number2} is {addition}.')
