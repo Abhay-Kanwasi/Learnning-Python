4103a2755c012daadff8b56b078b822bd94961f2
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-14 20:03:19 +0530

    Program 3 added

---
+++ 100 Program/Program 3.py
@@ -0,0 +1,8 @@
+# Product of two numbers
+
+number1 = int(input("Enter the nubmer : "))
+number2 = int(input("Enter the nubmer : "))
+
+product = number1 * number2
+
+print(f'Product of {number1} and {number2} is {product}.')
