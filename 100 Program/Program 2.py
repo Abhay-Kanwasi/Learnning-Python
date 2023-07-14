7303b59a629420355148906b35e61233cdeff4b1
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-14 20:03:07 +0530

    Program 2 added

---
+++ 100 Program/Program 2.py
@@ -0,0 +1,9 @@
+# Subtract two numbers 
+
+number1 = int(input("Enter the numbmer : "))
+number2 = int(input("Enter the numbmer : "))
+
+subtraciton = number1 - number2
+print("Subtracting number1 - number2")
+
+print(f'Subtraction of {number1} and {number2} is {subtraciton}')
