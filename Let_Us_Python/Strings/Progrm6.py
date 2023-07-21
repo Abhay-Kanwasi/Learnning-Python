c1968725debcd2ab8ac8238f9fbfea00b947035b
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-21 21:49:09 +0530

    output of the program

---
+++ Let_Us_Python/Strings/Progrm6.py
@@ -0,0 +1,12 @@
+# (d) What will be the output of the following program.
+
+s = 'HumptyDumpty'
+
+print('s=',s)
+print(s.isalpha())
+print(s.isdigit())
+print(s.isalnum())
+print(s.islower())
+print(s.isupper())
+print(s.startswith('Hump'))
+print(s.endswith('Dump'))
