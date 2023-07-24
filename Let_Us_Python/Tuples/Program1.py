e6fa6c1f399d765cafdaee1e3020d21d56895824
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-24 23:20:39 +0530

    program1 added program 2 initialised

---
+++ Let_Us_Python/Tuples/Program1.py
@@ -0,0 +1,4 @@
+# Suppose a data is represented as tuple (d, m, y). Write a program to create two date tuples and find the number of days between the two dates.
+
+date1 = (12, 12, 2018)
+date2 = (12, 10, 2020)
