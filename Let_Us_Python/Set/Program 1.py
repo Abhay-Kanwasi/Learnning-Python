792de78bb317d3c81279dc8ddc7512661b22b4b7
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-07-26 22:41:38 +0530

    set program 1 added

---
+++ Let_Us_Python/Set/Program 1.py
@@ -0,0 +1,4 @@
+# Set are like lists, with exception that they do not contain duplicate entries and unordered.
+
+c = {14,15,1,18,1} # use of duplicate value
+print(c) # printed an unordered list not like given
