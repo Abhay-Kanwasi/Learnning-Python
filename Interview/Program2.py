5e1c218fe2e5caa3369aee0deca4b6cfaf3877c2
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-02 14:57:30 +0530

    second question added

---
+++ Interview/Program2.py
@@ -0,0 +1,6 @@
+# Guess the output 
+
+name = 'Shantanu'
+print(name[2:8:-2])
+
+# Output will be not found because the -2 wil go from right to left for the search of 8 index which is on the right side of index that's why it will never found it in the first case.
