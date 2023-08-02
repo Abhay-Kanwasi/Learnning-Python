991e2e758e78cb4d67d9c97f5bd3ec170bfc40ff
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-02 17:39:03 +0530

    dictionary first program added

---
+++ Let_Us_Python/Dictionary/Program1.py
@@ -0,0 +1,10 @@
+# Create an empty dictionary and add three key-value pairs to it.
+
+dictionary = {} # Create an empty dictionary 
+
+# Add three key-value pairs to it.
+dictionary['aman'] = 5
+dictionary['raman'] = 7
+dictionary['chaman'] = 8
+
+print(dictionary)
