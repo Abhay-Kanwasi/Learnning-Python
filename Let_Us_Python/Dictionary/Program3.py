24eb2b58c7920c370038dcced7e32cee03a35c6e
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-04 12:08:15 +0530

    program 3 added

---
+++ Let_Us_Python/Dictionary/Program3.py
@@ -0,0 +1,20 @@
+# Write a Python function that takes a sentence as input and returns a dictionary with each unique word as the key and its frequency as the value.
+
+sentence = 'skdhf sdlf'
+def function(sentence):
+    pass
+
+a = '25'
+b = int(a)
+print(type(b))
+
+b = [1,2,3]
+print(b)
+
+c = [1,2,3]
+d = ()
+d = list(d)
+d.extend(c)
+d = tuple(d)
+print(d)
+print(type(d))
