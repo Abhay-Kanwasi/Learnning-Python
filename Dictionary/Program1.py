a81f181c698be159e018537e280a7fff6aef39f1
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-05 11:27:44 +0530

    first program added

---
+++ Dictionary/Program1.py
@@ -0,0 +1,7 @@
+# How to convert two list into dictionary
+
+keys = ['Abhay','Anirudh','Anita','Akshita']
+values = ['Kanwasi', 'Purohit', 'Negi', 'Rawat']
+
+full_name = dict(zip(keys,values))
+print(full_name)
