88af5dd4200b814fd6b6af628753f3ce8a835165
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-06 22:13:25 +0530

    map new question added

--- Let_Us_Python/Functional Programming/Map_function.py
+++ Let_Us_Python/Functional Programming/Map_function.py
@@ -2,14 +2,24 @@
 
 
 
-# Finding square root of all numbers in the list and returning a list of these roots.
+# # Question 1 : Finding square root of all numbers in the list and returning a list of these roots.
 
-list1 = [1,2,3,4]
+# list1 = [1,2,3,4]
 
-"""
-First Step : new_list = map(function, iterable) # Function : lambda x : x**2, Iterable : list1
-Second Step : new_list = map(lambda x : x**2, list1)
-Third Step : new_list = list(map(lambda x: x**2, list1)) # Convert it into list because it will give a iterable object to us
-"""
-new_list = list(map(lambda x: x**2, list1))
-print(new_list) # Because it will give us iterable result
+# """
+# First Step : new_list = map(function, iterable) # Function : lambda x : x**2, Iterable : list1
+# Second Step : new_list = map(lambda x : x**2, list1)
+# Third Step : new_list = list(map(lambda x: x**2, list1)) # Convert it into list because it will give a iterable object to us
+# """
+
+
+# new_list = list(map(lambda x: x**2, list1))
+# print(new_list) # Because it will give us iterable result
+
+
+# # Question 2 : Converting all characters in the list to uppercase and returning the uppercase letter
+
+# lowercase = ['a', 'b', 'c', 'd']
+# uppercase = list(map(lambda x : x.upper(), lowercase))
+# print(uppercase)
+
