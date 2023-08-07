8b9eb04660b48d627e273a1fb3929b19870c6ee4
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-07 12:41:48 +0530

    filter new question

--- Let_Us_Python/Functional Programming/Filter_Function.py
+++ Let_Us_Python/Functional Programming/Filter_Function.py
@@ -1,5 +1,12 @@
 # A filter operation applies a function to all the elements of a sequence. A sequence of those elements for which the function returns True.
 
-sequence = [2,4,4,6,6,5]
-filtered_sequence = list(filter(lambda x : x%2==0, sequence))
+# sequence = [2,4,4,6,6,5]
+# filtered_sequence = list(filter(lambda x : x%2==0, sequence))
+# print(filtered_sequence)
+
+
+# Question 1 : Checking whether each element in a list is an alphabet and returning a list of alphabets.
+
+sequence_for_question1 = ['2','a','g']
+filtered_sequence = list(filter(str.isalpha, sequence_for_question1))
 print(filtered_sequence)
