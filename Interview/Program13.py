fd7f4c8e703bddb25d6e9d4a6217f80e974ed0a5
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-08 09:54:15 +0530

    new question added

---
+++ Interview/Program13.py
@@ -0,0 +1,10 @@
+# Suppose we have a list. I want to add all my tuple elements into that list.
+
+list1 = []
+
+mytuple = (1,2,3,4)
+
+for element in mytuple:
+    list1.append(element)
+
+print(list1)
