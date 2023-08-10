0889dd6c5875a8fad899f26488bc1cde7489abc5
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-10 15:55:47 +0530

    dequeue completed

--- Collections/dequeue.py
+++ Collections/dequeue.py
@@ -10,6 +10,9 @@ d.appendleft(3) # ([3,1,2])
 # No need of right because it will already appending items to right
 d.popleft() # ([1,2])
 # No need of right because it will already poping items from right
-list1 = [2,4,1,8] # ([1,2,2,4,1,8])
-d.extend(list1)
+list1 = [2,4,1,8] 
+d.extend(list1) # ([1,2,2,4,1,8])
+
+d.rotate(1)
+
 print(d)
