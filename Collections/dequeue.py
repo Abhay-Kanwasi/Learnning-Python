f78b5fba72adc0d898c8da421636556b7e4ab3af
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-10 15:46:40 +0530

    dequeue added

---
+++ Collections/dequeue.py
@@ -0,0 +1,15 @@
+# Dequeue : It is an double ended queue. It can remove add or remove elements from both ends
+
+from collections import deque
+
+d = deque()
+
+d.append(1) # ([1])
+d.append(2) # ([1,2])
+d.appendleft(3) # ([3,1,2])
+# No need of right because it will already appending items to right
+d.popleft() # ([1,2])
+# No need of right because it will already poping items from right
+list1 = [2,4,1,8] # ([1,2,2,4,1,8])
+d.extend(list1)
+print(d)
