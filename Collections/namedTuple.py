edfdfc0958fbf62e5ffd147771ef6bb8fb2272ec
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-10 15:23:24 +0530

    namedtuple added

--- Collections/namedTuple.py
+++ Collections/namedTuple.py
@@ -1,1 +1,9 @@
-# namedTuple
+# nametuple : easy to create and light weight object similar to struct
+
+from collections import namedtuple 
+Point = namedtuple('Point', 'x,y')
+pt = Point(1,-4)
+print(pt) # Output : Point(x=1, y=4)
+print(pt.x, pt.y)   # Output : 1, -4
+
+print(pt[0] + pt[1])
