3f5055d57057a53b84209dfd81ee8a37fe159396
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-12 22:00:44 +0530

    program2 completed

--- JSON/program2.py
+++ JSON/program2.py
@@ -1,1 +1,15 @@
 # Convert python object to JSON data
+
+import json
+# a Python object (dict):
+python_obj = {
+  "name": "David",
+  "class":"I",
+  "age": 6  
+}
+print(type(python_obj))
+# convert into JSON:
+j_data = json.dumps(python_obj)
+
+# result is a JSON string:
+print(j_data)
