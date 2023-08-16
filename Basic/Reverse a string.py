342b157a16ecc34dbbcf2fc608465222e6952938
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-16 22:48:31 +0530

    reverse string

---
+++ Basic/Reverse a string.py
@@ -0,0 +1,5 @@
+# Write a program to reverse a given string without using any built-in functions.
+
+string = input("Enter the string : ")
+reverse_string = string[::-1]
+print(f'String : {string}\nReverse String : {reverse_string}')
