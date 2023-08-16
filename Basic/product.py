3fd7f5901176b6ef7104f88475c26ecd4a55175a
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-16 22:46:54 +0530

    product added and reverse string initialized

---
+++ Basic/product.py
@@ -0,0 +1,9 @@
+# Create a function that takes two numbers as input and returns their product.
+
+def product(a,b):
+    product = a * b
+    return product
+
+a = int(input('Enter the value of a : '))
+b = int(input('Enter the value of b : '))
+print(product(a,b))
