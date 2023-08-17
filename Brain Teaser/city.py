2c6866151261b9a6cb6fe79a53ceca9a5966d970
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-17 21:41:50 +0530

    completed

---
+++ Brain Teaser/city.py
@@ -0,0 +1,16 @@
+# Guess the output 
+
+city = 'Kraków'
+print(len(city))
+
+# Output : 7
+
+# Reason : This is because in the string city there is an character which is an accent mark and considered one more character beside o itself
+
+# K
+# r
+# a
+# k
+# o (base character)
+# ́ (combining character, accent mark)
+# w
