823cf2ed3a4cf4bc8d18c4ac59d61e2ec98a2720
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-19 23:14:45 +0530

    something added

---
+++ Interview/Program14GeekForGeeks.py
@@ -0,0 +1,6 @@
+# You have given a string. String will contain only lowercase letters and you have to find first unique character in that string.
+
+## For string = 'aabdddeffb' Ouptpu = e
+
+string = 'aabdddeffb'
+string1 = set(string)
