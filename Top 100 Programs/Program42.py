08da49991d554ba4bfcdf316eaa797f472600cb8
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-22 13:02:28 +0530

    absolute value program with 2 methods

---
+++ Top 100 Programs/Program42.py
@@ -0,0 +1,9 @@
+# Absolute value 
+
+number = int(input("Enter the number : "))
+# print(f'Absolute value of {number} is {abs(number)}.')
+
+if number > 0:
+    print(f'Absolute value of {number} is {number}.')
+else:
+    print(f'Absolute value of {number} is {-(number)}.')
