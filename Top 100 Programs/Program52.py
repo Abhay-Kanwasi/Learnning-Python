99d71131618b33f0996597f63a3425d622423ccf
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-26 11:17:39 +0530

    leap year

---
+++ Top 100 Programs/Program52.py
@@ -0,0 +1,7 @@
+# Leap year 
+
+year = int(input("Enter the year : "))
+if year%4==0 and year % 400==0 or year%100!=0:
+    print(f'{year} is leap year')
+else:
+    print(f'{year} is not leap year')
