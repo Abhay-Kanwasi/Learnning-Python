da614dbc4b27839d3aa34b6bd9bad3059a2eda91
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-26 12:54:54 +0530

    difference b/w two numbers

---
+++ Top 100 Programs/Program57.py
@@ -0,0 +1,9 @@
+# Difference between two numbers 
+
+number1 = int(input("Enter the number : "))
+number2 = int(input("Enter the number : "))
+
+if number1 != number2:
+    print("Both numbers are different from each other.")
+else:
+    print("Both numbers are same.")
