efc7d22e35acd1df81083a7d3b8bdc009dafdb4e
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-30 22:42:21 +0530

    new program added

---
+++ Top 100 Programs/Program59.py
@@ -0,0 +1,14 @@
+# Sum of digits of a number
+
+# string manipulation 
+number = int(input("Enter the number : "))
+str_number = str(number)
+sum_of_digits = 0
+for digit in str_number:
+    sum_of_digits += int(digit)
+print(sum_of_digits)
+
+# digit manipulation
+
+
+
