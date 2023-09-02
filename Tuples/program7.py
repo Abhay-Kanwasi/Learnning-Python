22fa486dca67cf501a94c96ed54b2ffa1b18d5c5
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-02 10:37:19 +0530

    4th element

---
+++ Tuples/program7.py
@@ -0,0 +1,5 @@
+# Write a Python program to get the 4th element from the last element of a tuple.
+
+elements = (2,3,4,5,6,7,1,9)
+fourth_element_from_last = elements[-4]
+print(fourth_element_from_last)
