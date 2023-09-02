66f9976d7412936cbd1b93413d391362cb9cd9e0
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-02 13:45:44 +0530

    element exist

---
+++ Tuples/program10.py
@@ -0,0 +1,10 @@
+# Check whether an element exist within a tuple or not.
+
+tuples = (2,3,4,1,9)
+
+find = int(input("Enter the number you want to find : "))
+
+if find in tuples:
+    print(True)
+else:
+    print(False)
