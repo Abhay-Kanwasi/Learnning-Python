70524f6b3fda989f1c743457c4d2e300838d98c2
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-02 10:17:55 +0530

    program 2 added

---
+++ Tuples/program2.py
@@ -0,0 +1,8 @@
+# Write a Python program to create a tuple with different data types.
+integer_type = 2
+float_type = 2.9
+character_type = 'a'
+boolean_type = True
+
+different_type_data_types = (integer_type, float_type, character_type, boolean_type)
+print(different_type_data_types)
