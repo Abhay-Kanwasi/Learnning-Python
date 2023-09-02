0d722b9dd0b5a7a8599113471e60fcfd578023ec
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-02 10:32:41 +0530

    string conversion

---
+++ Tuples/program6.py
@@ -0,0 +1,13 @@
+# convert a tuple to a string 
+
+list_of_tuples = ('a','b','h','i')
+string_of_tuples = ''.join(list_of_tuples)
+print(string_of_tuples)
+
+list_of_ch = ['a','b','h','i']
+string_of_list = ''.join(list_of_ch)
+print(string_of_list)
+
+set_of_char = {'a','b','h','i'}
+string_of_set = ''.join(set_of_char) #unordered so string is not in proper order
+print(string_of_set)
