adb306d630fed35dc7b645987bd521ce63be0b07
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-28 14:13:26 +0530

    new file created

---
+++ Basic Concepts/Something.py
@@ -0,0 +1,22 @@
+# start, end, step = input("Enter the start, end and step value for a range : ").split()
+
+# for i in range(int(start), int(end), int(step)):
+#     print(i, i**2, i**3)
+
+# contacts = {
+#     'Abhay' : 8057021541, 'Abhay2' : 8273576067,
+#     'Roman' : 6463461396, 'Sohan' : 8613563232
+# }
+
+# for name, phone_no in contacts.items():
+#     print(f'{name:15}: {phone_no:10d}')
+
+
+# Common tuple operations 
+# t = (12, 15, 13, 23, 22, 16)
+
+# print(min(t))
+# t = sorted(t)
+# print(t)
+# print(type(t))
+# print(sum(t))
