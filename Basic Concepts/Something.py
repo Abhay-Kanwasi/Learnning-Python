8924322e46b7ad66ebc80e26a30a2fa76875e9d1
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-28 23:12:49 +0530

    something added

--- Basic Concepts/Something.py
+++ Basic Concepts/Something.py
@@ -1,22 +1,91 @@
-# start, end, step = input("Enter the start, end and step value for a range : ").split()
+# Pack first 10 multiples of 10 into a tuple
 
-# for i in range(int(start), int(end), int(step)):
-#     print(i, i**2, i**3)
+tuple1 = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 
-# contacts = {
-#     'Abhay' : 8057021541, 'Abhay2' : 8273576067,
-#     'Roman' : 6463461396, 'Sohan' : 8613563232
-# }
+# Unpack the tuple into 10 variables, each holding 1 value.
+# a,b,c,d,e,f,g,h,i,j = tuple1
+# print(tuple1)
+# print(a,b,c,d,e,f,g,h,i,j)
 
-# for name, phone_no in contacts.items():
-#     print(f'{name:15}: {phone_no:10d}')
+# list = ['Shubham', 'Nisha', 'Sudha', ('Suresh',),('Rajesh',),'Radha']
+# boys = 0
+# girls = 0
+# for i in list:
+#     if isinstance(i, tuple):
+#         boys+=1
+#     else:
+#         girls+=1
+# print(boys)
+# print(girls)
+
+# from datetime import date
+
+# date1 = date(2012, 12, 12)
+# date2 = date(2023, 11, 11)
+
+# difference = date2 - date1
+# print(difference.days)
 
 
-# Common tuple operations 
-# t = (12, 15, 13, 23, 22, 16)
+# tuples = [('item1','22.2'),('item2', '2.4'),('item3', '3.2')]
+# print(sorted(tuples, key=lambda a: float(a[1]), reverse=True))
 
-# print(min(t))
-# t = sorted(t)
-# print(t)
-# print(type(t))
-# print(sum(t))
+# user = (
+#     ('Share name','TATA'),
+#     ('Date of purchase','20Dec2019'),
+#     ('Cost price','2.0LPA'),
+#     ('Number of shares','10'),
+#     ('Selling price','2.7LPA'),
+#     )
+
+set1 = {12,15,13,23}
+t = {'A', 'B', 'C'}
+
+set1.update(t)
+
+set1.add('H')
+print(set1)
+
+set1.remove(15)
+print(set1)
+
+set1.discard(101) # even element doesn't exist it's not giving me error.
+
+set1.clear()
+print(set1)
+
+set1 = {'Abhay', 'Akash','Bohimiya','Brad','Ammu'}
+
+set1 = list(set1)
+
+aset = set()
+bset = set()
+
+for name in set1:
+    if name[0] == 'A':
+        aset.add(name)
+    elif name[0] == 'B':
+        bset.add(name)
+    else:
+        continue
+
+print(aset)
+print(bset)
+
+##################################################
+
+import random
+
+numbers = set()
+count = 0
+for num in range(10):
+    numbers.add(random.randint(15,45))
+print(numbers)
+numbers = list(numbers)
+for num in numbers:
+    if num < 30:
+        count += 1
+    if num > 35:
+        numbers.remove(num)
+print(numbers)
+print(count)
